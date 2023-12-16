982564205fe39307aa6371b93ae23f681053f3a0
TetraTheta <37061801+TetraTheta@users.noreply.github.com>
2023-12-16 20:42:51 +0900

    Migrate to Node

--- README.md
+++ README.md
@@ -1,95 +1,105 @@
 # Blue Archive Sound Sorter
 
-Python scripts written for Blue Archive JP version.
+Extract BGM files from MediaPatch directory of Blue Archive JP.
 
-## BGM JP Sorter
+> **LEGAL NOTICE**  
+> You must not redistribute files from MediaPatch. This includes files created/converted from MediaPatch.
 
-Based on the information in the `MediaCatalog.json` file, it restores the original names of BGM files as simple numeric names.
+## Requirement
 
-### [BGM JP Sorter] Usage
+1. Node +20
+2. FFmpeg executable in `PATH`  
+   Only if you use `convert` subcommand
 
-```bash
-python 00-sorter.py [-i input] [-o output] [-c catalog] [-t theme_only]
-```
+## Usage
 
-* `input`: Path to the `MediaPatch` directory
- (Default: `MediaPatch`)
-* `output`: Directory where the BGM files will be copied with the name change (Default: `output`)
-* `catalog`: Path to the `MediaCatalog.json` file (Default: `MediaPatch/MediaCatalog.json`)
-* `theme_only`: Should we only process Theme audio files? (Default: `True`)
+### 1. Clone or download repository
 
-If you placed the `MediaPatch` directory in the path where this script is located, you only need to care about the `-t` switch.
-
-### [BGM JP Sorter] Requirements
-
-This script requires a `MediaPatch` directory that contains `MediaCatalog.json`. Place the `MediaPatch` directory on the same path as this script.
-
-```plaintext
-BlueArchiveSoundSorter.
-│  00-sorter.py
-│
-└─MediaPatch
-        MediaCatalog.json
-```
-
-### [BGM JP Sorter] Note
-
-The following BGM files are not included in the MediaPatch directory:  
-Therefore, the files below must be extracted from inside the APK or obtained in another way.
-
-* Theme 001 (`Theme_01.ogg`)
-* Theme 008 (`Theme_08.ogg`)
-* Theme 009 (`Theme_09.ogg`)
-* Theme 011 (`Theme_11.ogg`)
-* Theme 012 (`Theme_12.ogg`)
-* Theme 018 (`Theme_18.ogg`)
-* Theme 023 (`Theme_23.ogg`)
-* Theme 029 (`Theme_29.ogg`)
-* Theme 031 (`Theme_31.ogg`)
-* Theme 032 (`Theme_32.ogg`)
-* Theme 034 (`Theme_34.ogg`)
-* Theme 040 (`Theme_40.ogg`)
-* Theme 041 (`Theme_41.ogg`)
-* Theme 042 (`Theme_42.ogg`)
-* Theme 059 (`Theme_59_Title.ogg`)
-* Theme 154 (`Theme_154_Title.ogg`)
-
-## MP3 IDv3 Tagger
-
-Apply IDv3 tags to MP3 files based on pre-built information.
-
-## [MP3 IDv3 Tagger] Usage
+This is not NPM package. You have to either clone or download this repository. It is intentional.
 
 ```bash
-python 01-tagger.py [-i input_directory] [-d database_file]
+# Clone
+git clone https://github.com/TetraTheta/BlueArchiveSoundSorter
+# Download
+# Just click 'Code - Download ZIP' and extract it somewhere
 ```
 
-* `input_directory`: Directory path containing Blue Archive BGM files converted to MP3 format (Default: `mp3`)
-* `database_file`: Path of JSON file with MP3 tag information (Default: `asset/database.json`)
+### 2. Get MediaPatch from Blue Archive JP
 
-If you place the `mp3` directory in the path where this script is located, you can just execute the script without flags.
+If you experience problems logging in as a guest, try to install the Android Webview package.
 
-## [MP3 IDv3 Tagger] Requirements
+You must finish a tutorial (Battle with Wakamo & S.C.H.A.L.E. Recapture) to get the full MediaPatch directory. Files are downloaded while you are playing the tutorial. After finishing the tutorial, you can close the game.
 
-This script requires the pip packages `eyed3` and `python-magic-bin`.  
-This script also requires a directory containing Blue Archive BGM files converted to MP3 format. Place the directory with this script in the directory where it is located.
+Run these commands in the terminal.
 
 ```bash
-pip install -r requirements.txt
-# or
-pip install eyed3 python-magic-bin
+# cd to /sdcard
+cd /sdcard
+# (Optional) get root permission
+su
+# pack MediaPatch directory
+tar -czvf /sdcard/MediaPatch.tar.gz /sdcard/Android/data/com.YostarJP.BlueArchive/files/MediaPatch/
+# (Optional) copy OBB file
+cp /sdcard/Android/obb/com.YostarJP.BlueArchive/main.246192.com.YostarJP.BlueArchive.obb /sdcard/obb.zip
 ```
 
-```plaintext
-BlueArchiveSoundSorter.
-│  01-tagger.py
-│
-└─mp3
-        Theme_02.mp3
-        Theme_03.mp3
-        ...
+Now, copy `MediaPatch.tar.gz` and `obb.zip` file to your PC.
+
+Extract contents of `MediaPatch.tar.gz` to `BlueArchiveSoundSorter` like this:
+```
+/path/to/BlueArchiveSoundSorter/.source/MediaPatch/<lots of files>
 ```
 
-## [MP3 IDv3 Tagger] Note
+### 3. Install required packages
 
-This script will write IDv3 tags directly to MP3 files. Please make a backup of the original MP3 files before proceeding.
+Run this command in `BlueArchiveSoundSorter`.
+
+```bash
+npm ci
+```
+
+### 4. Run commands
+
+Now you can run commands of BlueArchiveSoundSorter.
+
+#### `sort`
+
+Renames BGM files based on data of 'MediaCatalog.json' file.
+
+```
+node ./index.mjs sort [-s|--source <path>] [-o|--output <path>] [-c|--catalog <file>] [-t|--theme-only <boolean>]
+```
+
+- `--source`, `-s`: Path which have 'MediaCatalog.json' and media files. (Default: `./.source/MediaPatch/`)
+- `--output`, `-o`: Path to save renamed media files. (Default: `./.source/RenamedOGG/`)
+- `--catalog`, `-c`: Path of 'MediaCatalog.json' (Default: `./.source/MediaPatch/MediaCatalog.json`)
+- `--theme-only`, `-t`: Process OST file only? (Default: `true`)
+
+If you followed the instructions above, you don't need to provide any options except for `--theme-only` which is your preference. Do note that if you provide `false` to `--theme-only`, it will take very long time to finish the process because it will try to *sort* other audio files like Character Voice etc.
+
+Some files are not in `MediaPatch` directory. In this case, they should be inside of `obb.zip`. That's why I instructed you to copy OBB file too.
+
+#### `convert`
+
+Convert OGG files to MP3 format (320kbps, 48000Hz)
+
+```
+node ./index.mjs convert [-s|--source <path>] [-o|--output <path>]
+```
+
+- `--source`, `-s`: Path which have OGG files to convert. (Default: `./.source/RenamedOGG/`)
+- `--output`, `-o`: Path to save converted MP3 files. (Default: `./.source/MP3/`)
+
+I know that this is overkill to convert OGG files to MP3 format, but I couldn't find any way to apply metadata(tag) to OGG file.
+
+#### `tag`
+
+Apply IDv3 tags to MP3 files and rename them. This is irreversible!
+
+```
+node ./index.mjs sort [-s|--source <path>] [-d|--database <file>] [-a|--album-art <file>]
+```
+
+- `--source`, `-s`: Path which contains MP3 files for tagging. (Default: `./.source/MP3/`)
+- `--database`, `-d`: YAML file which have tag information. (Default: `./asset/tag_db.yml`)
+- `--album-art`, `-a`: Album Art file. (Default: `./asset/game_ost_album_art.webp`)
--- index.mjs
+++ index.mjs
@@ -1,7 +1,13 @@
 #!/usr/bin/env node
+import chalk from 'chalk'; // chalk is from commander!
+import { exec, execSync } from 'child_process'
 import { Command } from 'commander'
 import fs from 'fs'
-import chalk from 'chalk' // chalk is from commander!
+import yaml from 'js-yaml'
+import { compare } from 'natural-orderby'
+import NodeID3 from 'node-id3'
+import path from 'path'
+
 chalk.level = 1 // Use color in VS Code Debug Window
 const program = new Command()
 program
@@ -11,8 +17,8 @@ program
 // Sort
 program
   .command('sort')
-  .description('Renames BGM files based on \'MediaCatalog.json\' file.')
-  .option('-s, --source <path>', 'Path which includes \'MediaCatalog.json\' and media files', './.source/MediaPatch/')
+  .description('Renames BGM files based on data of \'MediaCatalog.json\' file.')
+  .option('-s, --source <path>', 'Path which have \'MediaCatalog.json\' and media files', './.source/MediaPatch/')
   .option('-o, --output <path>', 'Path to save renamed media files', './.source/RenamedOGG/')
   .option('-c, --catalog <file>', 'Path of \'MediaCatalog.json\'', './.source/MediaPatch/MediaCatalog.json')
   .option('-t, --theme-only', 'Process OST file only?', true)
@@ -21,7 +27,8 @@ program
     if (!isDir(options.source)) error('Given source path is not valid directory: ', options.source)
     if (!isFile(options.catalog)) error('Given catalog path is not valid file: ', options.catalog)
     // create dictionary for file (name, size)
-    let fileDict = {}
+    console.log('Scanning MediaPatch dictionary... This will take a while. Please wait.')
+    const fileDict = {}
     fs.readdirSync(options.source, {withFileTypes: true}).forEach(f => {
       // f is Dirent
       if (!f.isFile()) return // extra file check
@@ -29,38 +36,105 @@ program
       if (ext == 'dat' || ext == 'hash' || ext == 'json') return
       fileDict[f.name] = fs.statSync(f.path + f.name).size
     })
-    console.table(fileDict)
+    // create array for save files already warned
+    const noticed = {}
     // read catalog
-    const catalog = fs.readFileSync(options.catalog, 'utf8')
-    const catalogData = JSON.parse(catalog)
+    const catalog = JSON.parse(fs.readFileSync(options.catalog, 'utf8'))
+    // create directory for output
+    if (!fs.existsSync(options.output)) {
+      fs.mkdirSync(options.output, {recursive: true})
+    }
+    // start job
+    console.log('Starting sort...')
     // loop catalog for sorting
-    catalogData.Table.forEach(item => {
-      if (options.themeOnly && item.path.startsWith(''))
-    })
+    for (const item of Object.values(catalog['Table']).sort(compare())) {
+      for (const [fk, fv] of Object.entries(fileDict).sort(compare())) {
+        if (options.themeOnly && !item['path'].startsWith('Audio/BGM/')) continue
+        if (item['IsPrologue'] === true && !noticed[item['FileName']]) {
+          warn(`${item['FileName']} is not in MediaPatch directory! Extract it from APK or OBB file.`)
+          noticed[item['FileName']] = true
+          continue
+        }
+        if (parseInt(fv) === parseInt(item['Bytes'])) {
+          const outputPath = path.join(path.resolve(options.output, item['FileName']))
+          console.log(outputPath)
+          fs.copyFileSync(path.resolve(options.source, fk), outputPath)
+        }
+      }
+    }
+    console.log('Job done.')
   })
 // Convert
 program
   .command('convert')
-  .description('Convert OGG files to MP3 format')
-  .option('-s, --source <path>', 'Path which includes OGG files', './.source/RenamedOGG/')
+  .description('Convert OGG files to MP3 format (320kbps, 48000Hz)')
+  .option('-s, --source <path>', 'Path which have OGG files to convert', './.source/RenamedOGG/')
   .option('-o, --output <path>', 'Path to save converted MP3 files', './.source/MP3/')
   .action((options) => {
     // check options
     if (!isDir(options.source)) error('Given source path is not valid directory: ', options.source)
-    console.log('convert...')
+    // check FFmpeg
+    isFFmpegInPath().catch(() => {
+      error('FFmpeg is not found in PATH. Aborting...')
+      process.exit(1)
+    })
+    // create directory for output
+    if (!fs.existsSync(options.output)) {
+      fs.mkdirSync(options.output, {recursive: true})
+    }
+    // convert files
+    fs.readdirSync(options.source).filter(file => file.toLowerCase().endsWith('.ogg')).forEach(file => {
+      const oggPath = path.join(options.source, file)
+      const mp3Path = path.join(options.output, path.basename(file, '.ogg') + '.mp3')
+
+      try {
+        console.log('Converting ' + chalk.yellow(file) + ' ...')
+        execSync(`ffmpeg -i "${oggPath}" -c:a libmp3lame -b:a 320k -ar 48000 "${mp3Path}"`, {stdio: 'ignore'})
+      } catch (err) {
+        error(`Failed to convert ${oggPath}:\n${err.stderr.toString()}`)
+      }
+    })
   })
 // Tag
 program
   .command('tag')
-  .description('Apply IDv3 tags to MP3 files. This is irreversible!')
-  .option('-s, --source <path>', 'Path which includes MP3 files for tagging', './.source/MP3/')
+  .description('Apply IDv3 tags to MP3 files and rename them. This is irreversible!')
+  .option('-s, --source <path>', 'Path which contains MP3 files for tagging', './.source/MP3/')
   .option('-d, --database <file>', 'YAML file which have tag information', './asset/tag_db.yml')
+  .option('-a, --album-art <file>', 'Album Art file', './asset/game_ost_album_art.webp')
   .action((options) => {
     // check options
     if (!isDir(options.source)) error('Given source path is not valid directory: ', options.source)
     if (!isFile(options.database)) error('Given database path is not valid file: ', options.catalog)
-    console.log('tag...')
-    console.log(options.source + ' ' + options.database)
+    // read database
+    const db = yaml.load(fs.readFileSync(options.database, 'utf8'))
+    fs.readdirSync(options.source).filter(file => file.toLowerCase().endsWith('.mp3')).forEach(file => {
+      for (const entry of db['database']) {
+        //console.log(entry)
+        if (path.basename(file, '.mp3') === entry['name_old']) {
+          console.log('Applying tag to ' + chalk.yellow(file) + ' ...')
+          const aaBuffer = fs.readFileSync(options.albumArt)
+          const tag = {
+            title: entry['title'],
+            artist: entry['artist'],
+            album: db['common']['album'],
+            performerInfo: db['common']['album_artist'],
+            trackNumber: entry['track'],
+            year: db['common']['year'],
+            image: {
+              mime: 'image/webp',
+              imageBuffer: aaBuffer
+            }
+          }
+          if (NodeID3.write(tag, path.join(options.source, file)) != true) {
+            error('Failed to write IDv3 tag to ', file)
+            return
+          }
+          const validName = entry['name_new'].replace(/[/\\?%*:|"<>]/g, '')
+          fs.renameSync(path.join(options.source, file), path.join(options.source, validName + '.mp3'))
+        }
+      }
+    })
   })
 program.parse()
 
@@ -73,7 +147,7 @@ program.parse()
  * @param {*} yellow anything to print as yellow text
  */
 function error(msg, yellow = '') {
-  console.error(chalk.red('ERR') + chalk.reset(': ' + msg) + chalk.yellow(yellow))
+  console.error(chalk.red('ERROR') + chalk.reset(': ' + msg) + chalk.yellow(yellow))
 }
 /**
  * Get extension from file name
@@ -107,4 +181,25 @@ function isFile(path) {
   } catch (err) {
     return false
   }
+}
+/**
+ * 
+ */
+function isFFmpegInPath() {
+  return new Promise((resolve, reject) => {
+    exec('ffmpeg -version', (error, stdout, stderr) => {
+      if (error) {
+        reject(false)
+      } else {
+        resolve(true)
+      }
+    })
+  })
+}
+/**
+ * Print warning message
+ * @param {*} msg message to print
+ */
+function warn(msg) {
+  console.error(chalk.yellow('WARN') + chalk.reset(': ' + msg))
 }
